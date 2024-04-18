45f7c805c8279fefb8c9722807f1ecd9b81a673f
Stephen Markovic <stephen.markovic@gmail.com>
2024-04-18 18:58:32 -0400

    Update api_keys.py

--- api_keys.py
+++ api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "aa89c4200aaa987a131ae9aec08927ce"
+weather_api_key = ""
 
 # Geoapify API Key
-geoapify_key = "88199010ba4d4803ad0bf8ad4105a0ca"
+geoapify_key = ""
